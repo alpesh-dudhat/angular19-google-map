9694f70e0240346c6bdd9f3551d4a8f6f0d7f681
Alpesh Dudhat <alpeshdudhat2010@gmail.com>
2025-04-15 10:08:02 +0100

    feat: add Angular animations support and enhance map tools functionality with distance mode toggle

--- src/app/shared/components/map-tools/map-tools.component.ts
+++ src/app/shared/components/map-tools/map-tools.component.ts
@@ -1,11 +1,24 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { IconComponent } from '../../../icons/icon.component';
 
 @Component({
   selector: 'app-map-tools',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,IconComponent],
   templateUrl: './map-tools.component.html',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class MapToolsComponent {
   @Input() isRouteMode: boolean = false;
@@ -13,5 +26,15 @@ export class MapToolsComponent {
 
   @Output() toggleRouteMode = new EventEmitter<void>();
   @Output() clearRoute = new EventEmitter<void>();
+
+  showInfo = signal(false);
+
+  onDistanceModeToggle() {
+    this.toggleRouteMode.emit();
+    if (!this.isRouteMode) {
+      this.showInfo.set(true);
+      setTimeout(() => this.showInfo.set(false), 3000);
+    }
+  }
 
 }
